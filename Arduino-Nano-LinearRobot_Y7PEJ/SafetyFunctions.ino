561423f5d8f8f8cae492b0f04c781f094cc46685
pejhej <36162293+pejhej@users.noreply.github.com>
2018-05-28 10:27:43 +0000

    Add files via upload

---
+++ Arduino-Nano-LinearRobot_Y7PEJ/SafetyFunctions.ino
@@ -0,0 +1,74 @@
+/*** All the safety functions ***/
+
+
+
+//Set the 
+void stopMotors() {
+  mainCommand = stopRobot;
+}
+
+
+
+
+
+
+
+/**
+   Checks the limit switches and sets the appropriate bools
+*/
+bool limitTrigged()
+{
+  bool limitIsTrigged = false;
+
+  //Check the X home switch
+  if (!digitalRead(X_HOME_SWITCH))
+  {
+    xHomeSwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    xHomeSwitch = false;
+  }
+
+  //Check the X outer switch
+  if (!digitalRead(X_OUTER_SWITCH))
+  { 
+    xAwaySwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    xAwaySwitch = false;
+  }
+
+  //Check the Y Home switch
+  if (!digitalRead(Y_HOME_SWITCH))
+  { 
+    yHomeSwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    yHomeSwitch = false;
+  }
+
+  //Check the Y outer switch
+  if (!digitalRead(Y_OUTER_SWITCH))
+  {
+    yAwaySwitch = true;
+    limitIsTrigged = true;
+  }
+  else
+  {
+    yAwaySwitch = false;
+  }
+
+
+  return limitIsTrigged;
+}
+
+
+
+
+
---
+++ Arduino-Nano-LinearRobot_Y7PEJ/addOnFunctions.ino
@@ -0,0 +1,79 @@
+
+
+
+
+  /**
+   Check failure and return the given failure state
+*/
+state checkFailure()
+{
+  state returnState = fail;
+
+  if (xHomeSwitch || xAwaySwitch || yHomeSwitch || yAwaySwitch )
+  {
+    inState = linearBotLimitTrigged;
+  }
+
+  return returnState;
+}
+/**
+   Returns true if the robot is in same position as new-position. Ergo no moving required
+*/
+boolean robotInPosition()
+{
+  boolean inPos = false;
+  if ((currentPos.x == newPos.x) && (currentPos.y == newPos.y))
+  {
+    inPos = true;
+  }
+
+  return inPos;
+}
+
+/**
+ * Update the status to given status and notify over serial com
+ */
+void updateStatusAndNotify(byte state) {
+
+  if (inState != state) 
+  {
+    inState = state;
+    sendInt(inState);
+  }
+}
+
+
+
+
+
+/**
+   Toggle the bool given to this function
+*/
+boolean toggleBool(boolean toggleThis)
+{
+
+  //Check if its high
+  if (toggleThis)
+  {
+    toggleThis = false;
+  }
+  else if (!toggleThis)
+  {
+    toggleThis = true;
+  }
+
+  return toggleThis;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
